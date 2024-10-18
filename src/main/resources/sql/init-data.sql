934b2c2fd839f99b116e2b942317a4dcd69917cb
= <baongabk@gmail.com>
2024-10-18 22:32:41 +0700

    Init schema and sample user data

---
+++ src/main/resources/sql/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO CRYPTO.USERS (username, email, usdt_balance, ethusdt_balance, btcusdt_balance, dt_created)
+VALUES
+('john_terry', 'john.terry@example.com', 50000.00, 0, 0, CURRENT_TIMESTAMP),
+('jane_smith', 'jane.smith@example.com', 50000.00, 0, 0, CURRENT_TIMESTAMP);
+
